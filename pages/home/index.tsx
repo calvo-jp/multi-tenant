f513ae12ffe0deb4cdb14037963b60adc757f2af
calvo-jp <calvojp92@gmail.com>
2022-12-10 17:05:25 +0800

    add badge

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -30,34 +30,46 @@ export default function Index({ data }: Props) {
       </Head>
 
       <main className="p-16">
-        <div className="mx-auto max-w-[600px]">
-          <ul className="flex flex-col gap-2">
-            {data.map(({ id, name, email, username }) => {
-              return (
-                <li key={id}>
-                  <a
-                    href={`http://${username}.localhost:5000`}
-                    className="flex items-center gap-4 rounded-md border border-neutral-200 p-4 outline-none transition-colors duration-300 hover:border-neutral-300 focus:border-sky-200"
-                  >
-                    <div className="overflow-hidden rounded-full">
-                      <Image
-                        src={`https://i.pravatar.cc/300?u=${id}`}
-                        width={48}
-                        height={48}
-                        alt=""
-                        draggable={false}
-                      />
-                    </div>
+        <div className="mx-auto max-w-[690px]">
+          <div className="flex items-center gap-2">
+            <h1 className="font-serif text-3xl font-bold tracking-[0.025em] text-neutral-800">
+              Authors
+            </h1>
 
-                    <div>
-                      <div className="text-xl font-semibold text-neutral-800">{name}</div>
-                      <div className="text-sm text-neutral-700">{email}</div>
-                    </div>
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
+            <div className="rounded-lg bg-purple-200 py-[2px] px-[4px] text-xs font-semibold leading-[1.15] text-purple-800">
+              {data.length}
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <ul className="flex flex-col gap-2">
+              {data.map(({ id, name, email, username }) => {
+                return (
+                  <li key={id}>
+                    <a
+                      href={`http://${username}.localhost:5000`}
+                      className="flex items-center gap-4 rounded-md border border-neutral-200 p-4 outline-none transition-colors duration-300 hover:border-neutral-300 focus:border-sky-200"
+                    >
+                      <div className="overflow-hidden rounded-full">
+                        <Image
+                          src={`https://i.pravatar.cc/300?u=${id}`}
+                          width={48}
+                          height={48}
+                          alt=""
+                          draggable={false}
+                        />
+                      </div>
+
+                      <div>
+                        <div className="text-xl font-semibold text-neutral-800">{name}</div>
+                        <div className="text-sm text-neutral-700">{email}</div>
+                      </div>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </main>
     </>
